eaf8c4826ca124ab93168fc17d41d9b4860ec7e5
Ryan Temple <ryantemple145@gmail.com>
2015-06-06 21:58:01 +0100

    Initial commit for deleting rollbacked releases

--- tasks/rollback/finish.js
+++ tasks/rollback/finish.js
@@ -11,6 +11,23 @@ module.exports = function (gruntOrShipit) {
 
   function task() {
     var shipit = init(utils.getShipit(gruntOrShipit));
+
+    // Remove rollbacked release if desired
+    if(shipit.config.deleteOnRollback){
+      var currentReleaseIndex = releases.indexOf(currentRelease);
+      var rollbackReleaseIndex = currentReleaseIndex - 1;
+
+      var rollbackDirName = releases[rollbackReleaseIndex];
+
+      if(!rollbackDirName)
+        throw new Error ("Cannot find rollback release to delete");
+
+      shipit.log('Deleting release %s.', rollbackDirName);
+
+      var command = "rm -rf " + shipit.releasesPath + "/" + rollbackDirName;
+      shipit.remote(command);
+      shipit.log('Removed release %s', rollbackDirName);
+    }
     shipit.emit('rollbacked');
   }
 };
