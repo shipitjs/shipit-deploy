04025776629afc1ac4b680795fb9a52b04485e21
Yvem <jutard@lemonde.fr>
2015-12-11 18:18:49 +0100

    add option to work on a pre-fetched workspace

--- test/unit/tasks/deploy/fetch.js
+++ test/unit/tasks/deploy/fetch.js
@@ -37,40 +37,54 @@ describe('deploy:fetch task', function () {
     shipit.local.restore();
   });
 
-  it('should create workspace, create repo, checkout and call sync', function (done) {
-    shipit.start('deploy:fetch', function (err) {
-      if (err) return done(err);
-      expect(mkdirpMock).to.be.calledWith('/tmp/workspace');
-      expect(shipit.local).to.be.calledWith('git init', {cwd: '/tmp/workspace'});
-      expect(shipit.local).to.be.calledWith('git remote', {cwd: '/tmp/workspace'});
-      expect(shipit.local).to.be.calledWith(
-        'git remote add shipit git://website.com/user/repo',
-        {cwd: '/tmp/workspace'}
-      );
-      expect(shipit.local).to.be.calledWith('git fetch shipit -p --tags', {cwd: '/tmp/workspace'});
-      expect(shipit.local).to.be.calledWith('git checkout master', {cwd: '/tmp/workspace'});
-      expect(shipit.local).to.be.calledWith('git branch --list master', {cwd: '/tmp/workspace'});
-      done();
+  context('when a fetch is required', function () {
+    it('should create workspace, create repo, checkout and call sync', function (done) {
+      shipit.start('deploy:fetch', function (err) {
+        if (err) return done(err);
+        expect(mkdirpMock).to.have.been.calledWith('/tmp/workspace');
+        expect(shipit.local).to.have.been.calledWith('git init', {cwd: '/tmp/workspace'});
+        expect(shipit.local).to.have.been.calledWith('git remote', {cwd: '/tmp/workspace'});
+        expect(shipit.local).to.have.been.calledWith(
+           'git remote add shipit git://website.com/user/repo',
+           {cwd: '/tmp/workspace'}
+        );
+        expect(shipit.local).to.have.been.calledWith('git fetch shipit -p --tags', {cwd: '/tmp/workspace'});
+        expect(shipit.local).to.have.been.calledWith('git checkout master', {cwd: '/tmp/workspace'});
+        expect(shipit.local).to.have.been.calledWith('git branch --list master', {cwd: '/tmp/workspace'});
+        done();
+      });
+    });
+
+    it('should create workspace, create repo, checkout shallow and call sync', function (done) {
+      shipit.config.shallowClone = true;
+
+      shipit.start('deploy:fetch', function (err) {
+        if (err) return done(err);
+        expect(shipit.local).to.have.been.calledWith('rm -rf /tmp/workspace');
+        expect(mkdirpMock).to.have.been.calledWith('/tmp/workspace');
+        expect(shipit.local).to.have.been.calledWith('git init', {cwd: '/tmp/workspace'});
+        expect(shipit.local).to.have.been.calledWith('git remote', {cwd: '/tmp/workspace'});
+        expect(shipit.local).to.have.been.calledWith(
+           'git remote add shipit git://website.com/user/repo',
+           {cwd: '/tmp/workspace'}
+        );
+        expect(shipit.local).to.have.been.calledWith('git fetch --depth=1 shipit -p --tags', {cwd: '/tmp/workspace'});
+        expect(shipit.local).to.have.been.calledWith('git checkout master', {cwd: '/tmp/workspace'});
+        expect(shipit.local).to.have.been.calledWith('git branch --list master', {cwd: '/tmp/workspace'});
+        done();
+      });
     });
   });
 
-  it('should create workspace, create repo, checkout shallow and call sync', function (done) {
-    shipit.config.shallowClone = true;
-
-    shipit.start('deploy:fetch', function (err) {
-      if (err) return done(err);
-      expect(shipit.local).to.be.calledWith('rm -rf /tmp/workspace');
-      expect(mkdirpMock).to.be.calledWith('/tmp/workspace');
-      expect(shipit.local).to.be.calledWith('git init', {cwd: '/tmp/workspace'});
-      expect(shipit.local).to.be.calledWith('git remote', {cwd: '/tmp/workspace'});
-      expect(shipit.local).to.be.calledWith(
-        'git remote add shipit git://website.com/user/repo',
-        {cwd: '/tmp/workspace'}
-      );
-      expect(shipit.local).to.be.calledWith('git fetch --depth=1 shipit -p --tags', {cwd: '/tmp/workspace'});
-      expect(shipit.local).to.be.calledWith('git checkout master', {cwd: '/tmp/workspace'});
-      expect(shipit.local).to.be.calledWith('git branch --list master', {cwd: '/tmp/workspace'});
-      done();
+  context('when the workspace is already fetched', function () {
+    it('should do nothing', function (done) {
+      shipit.config.preFetched = true; // overwrite
+      shipit.start('deploy:fetch', function (err) {
+        if (err) return done(err);
+        expect(mkdirpMock).to.not.have.been.called;
+        expect(shipit.local).to.not.have.been.called;
+        done();
+      });
     });
   });
 });
