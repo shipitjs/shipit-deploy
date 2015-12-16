0e2d3204de9838a3aaf65cdc3d9d16ed73521891
Jeremy Vincent <JVincent@wolters-kluwer.fr>
2015-12-16 09:25:07 +0100

    init default values for currentPath and relesesPath options

--- tasks/deploy/publish.js
+++ tasks/deploy/publish.js
@@ -25,6 +25,12 @@ module.exports = function (gruntOrShipit) {
     function updateSymbolicLink() {
       shipit.log('Publishing release "%s"', shipit.releasePath);
 
+      if ( shipit.config.releasesPath == undefined )
+        shipit.config.releasesPath = 'releases';
+
+      if ( shipit.config.currentPath == undefined )
+        shipit.config.currentPath = 'current';
+
       var relativeReleasePath = path.join(shipit.config.releasesPath, shipit.releaseDirname);
 
       return shipit.remote('cd ' + shipit.config.deployTo + ' && ln -nfs ' + relativeReleasePath + ' ' + shipit.config.currentPath + '')
